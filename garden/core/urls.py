37604a6837785c3d5f309880f87c31ee305dc5e5
Luis saavedra <l.saavedra@duocuc.cl>
2022-08-09 16:01:54 -0400

    Modificación navbar, Creación Footer

--- garden/core/urls.py
+++ garden/core/urls.py
@@ -1,9 +1,10 @@
 """IMPORTACIÓN DE PATH Y DE LAS FUNCIONES CREADAS EN VIEWS INICIALMENTE SE COPIA LA INFORMACIÓN DEL URLS UBICADO EN EL PROYECTO."""
 from django.urls import path
-from .views import index,registro
+from .views import home,registro 
 """MODIFICACIÓNDE PATH, NO SE LE ASIGNA UN NOMBRE DE RUTA APRA INGRESAR EN EL NAVEGADOR, EL PATH VAB A BUSCAR A LA FUNCIÓN HOME DE VIEWS.PY, Y EL RENDER EJECUTA LA RUTA, SE LE OTORGA UN ALIAS A LA URL."""
 urlpatterns = [
-    path('', index, name="index"),
+    path('', home, name="home"),
     path('registro/', registro, name="registro"),
+    
 
 ]
--- garden/core/views.py
+++ garden/core/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 """CREACIÓN DE CONTROLADORES QUE MOSTRARÁN LOS TEMPLATES EN EL NAVEGADOR, RENDER IRÁ EN LA BUSQUEDA DE LA RUTA DEL TEMPLATE."""
-def index(request):
+def home(request):
 
-    return render(request, 'core/index.html')
+    return render(request, 'core/home.html')  
 
 def registro(request):
 
-    return render(request, 'core/registro.html')    
+    return render(request, 'core/registro.html')  
+ 
+
